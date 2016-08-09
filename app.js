a8c0f4e05b1f76a53a6c4d4eed5af4261f064e17
mikat <mika2000.10@gmail.com>
2016-08-09 14:41:21 +0200

    Revert "added json"
    This reverts commit d55395b95b0f08d427dcb77901b77f3f44d67ed8.

--- app.js
+++ app.js
@@ -9,8 +9,6 @@ var routes = require('./routes/index');
 
 var app = express();
 
-app.locals.mainjson = require('./main.json');
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
