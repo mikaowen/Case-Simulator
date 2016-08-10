286f97cc6e022ccac18eaf970d7b292bf0cdec23
mikat <mika2000.10@gmail.com>
2016-08-10 12:07:21 +0200

    router.post

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,10 @@ router.get('/', function(req, res, next) {
   res.render('index', {});
 });
 
+router.post('/postjson', function(req, res, next) {
+  /*if (!req.body.obj) {res.status(404).send()}
+  if (!maindata[req.body.obj]) {res.status(404).send()}*/
+  console.log(maindata[req.body.obj]);
+});
+
 module.exports = router;
