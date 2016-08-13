11fba707e30c0aaaf078e432b0d508485700f680
mikat <mika2000.10@gmail.com>
2016-08-13 15:46:13 +0200

    router.post stuff

--- routes/index.js
+++ routes/index.js
@@ -9,28 +9,24 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/postjson', function(req, res, next) {
-  if (!req.body.obj || !mainjson[req.body.obj])
-    res.status(404).send
-  console.log(mainjson[req.body.obj]);
-});
-
-router.post("/get_skin", function(req, res, next) {
-  if (!req.body.obj || !mainjson[req.body.obj])
-    res.status(404).send
+  if (!req.body.obj||!mainjson[req.body.obj]) {res.status(404).send()}
   res.send(mainjson[req.body.obj]);
 });
 
 router.post("/nav_json", function(req, res, next) {
-  if (!req.body.obj || !mainjson[req.body.obj] || !mainjson)
-    res.status(404).send
-  if (!req.body.obj instanceof Array)
-    res.status(400).send
+  if (!req.body.obj || !mainjson[req.body.obj] || !mainjson) {res.status(404).send()}
+  if (!req.body.obj instanceof Array) {res.status(400).send()}
 
   var obj = mainjson;
   for (var i = 0; i < req.body.obj.length; i++) {
     obj = obj[req.body.obj[i]];
   }
   res.send(obj);
+});
+
+router.post('/getskin', function(req, res, next) {
+  if (!req.body.skins||!mainjson.skins[req.body.skin]) {res.status(404).send()}
+  res.send(mainjson.skins[req.body.skin]);
 });
 
 module.exports = router;
